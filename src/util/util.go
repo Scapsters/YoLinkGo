ee2728bf6b68b9bcd31f4d6e66bb5a5a77d8e1c5
Scapsters <scotty.happy@gmail.com>
2025-11-15 21:09:24 -0500

    add tomap util

--- src/util/util.go
+++ src/util/util.go
@@ -1,6 +1,10 @@
 package utils
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
 // Current time in milliseconds
 func Time() int64 {
@@ -10,4 +14,16 @@ func Time() int64 {
 // Current time in seconds
 func TimeSeconds() int64 {
 	return time.Now().UTC().UnixMilli() / 1000
+}
+
+func ToMap(data any) (map[string]any, error) {
+    marshalled, err := json.Marshal(data)
+    if err != nil {
+        return nil, fmt.Errorf("error while marshalling %v: %w", data, err)
+    }
+    var mapped map[string]any
+    if err := json.Unmarshal(marshalled, &mapped); err != nil {
+        return nil, fmt.Errorf("error while unmarshalling %v with intermediate value %v: %w", data, marshalled, err)
+    }
+    return mapped, nil
 }
