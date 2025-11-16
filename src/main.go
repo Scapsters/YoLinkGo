13f2b520c5aa715c1cf4bb2a2447b1700de1f067
Scapsters <scotty.happy@gmail.com>
2025-11-16 13:28:40 -0500

    rework data collection, add scheduler

--- src/main.go
+++ src/main.go
@@ -12,6 +12,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/go-co-op/gocron/v2"
 	"github.com/joho/godotenv"
 )
 
@@ -24,7 +25,6 @@ func main() {
 		log.Fatal("fatal:", err)
 	}
 }
-
 func run() error {
 	// Connect to DB
 	dbConnection, err := mysql.NewMySQLConnection("root:101098@tcp(127.0.0.1:3306)/")
@@ -64,54 +64,134 @@ func run() error {
 	// Store devices
 	for _, device := range result.Data.Devices {
 		stores.Devices.Add(data.Device{
-			Kind: device.Kind,
-			Name: device.Name,
-			Token: device.Token,
-			ID: device.DeviceID,
+			Kind:      device.Kind,
+			Name:      device.Name,
+			Token:     device.Token,
+			ID:        device.DeviceID,
 			Timestamp: utils.TimeSeconds(),
 		})
 	}
 
-	// Query device info
-	ID := "d88b4c01000277a9"
-	devices, err := stores.Devices.Get(data.DeviceFilter{
-		ID: &ID,
-	})
+	// create a scheduler
+	s, err := gocron.NewScheduler()
+	if err != nil {
+		return fmt.Errorf("error creating scheduler: %w", err)
+	}
+
+	// add a job to the scheduler
+	j, err := s.NewJob(
+		gocron.DurationJob(
+			5*time.Minute,
+		),
+		gocron.NewTask(
+			func() {
+				fmt.Println("starting")
+				gatherTHData(stores, yoLinkConnection)
+			},
+		),
+	)
+	if err != nil {
+		return fmt.Errorf("error creating job: %w", err)
+	}
+	// each job has a unique id
+	fmt.Println(j.ID())
+
+	// start the scheduler
+	s.Start()
+	fmt.Println(s.JobsWaitingInQueue())
+
+	// block until you are ready to shut down
+	time.Sleep(24 * time.Hour)
+
+	// when you're done, shut it down
+	err = s.Shutdown()
+	if err != nil {
+		return fmt.Errorf("error shutting down: %w", err)
+	}
+
+	return nil
+}
+
+func gatherTHData(stores db.StoreCollection, yoLinkConnection *sensors.YoLinkConnection) error {
+	// Query devices
+	kind := "THSensor"
+	devices, err := stores.Devices.Get(data.DeviceFilter{Kind: &kind})
 	if err != nil {
 		return fmt.Errorf("error while seraching for device: %w", err)
 	}
 	if len(devices) == 0 {
 		return fmt.Errorf("device not found")
 	}
-	device := devices[0]
 
-	deviceState, err := sensors.MakeYoLinkRequest[sensors.BUDP](yoLinkConnection, sensors.SimpleBDDP{Method: sensors.THSensorGetState, TargetDevice: &device.ID, Token: &device.Token})
-	if err != nil {
-		return fmt.Errorf("error while quering device: %w", err)
-	}
-
-	dataMap, err := utils.ToMap[any](deviceState.Data)
-	if err != nil {
-		return fmt.Errorf("error converting data %v: %w", deviceState.Data, err)
-	}
-    eventTimestamp, err := time.Parse(time.RFC3339Nano, dataMap["reportAt"].(string))
-	if err != nil {
-		return fmt.Errorf("error converting time %v to epoch seconds: %w", dataMap["reportedAt"], err)
-	}
-	targetFields := []string{"temperature", "humidity"}
-	for _, fieldName := range targetFields {
-		err := stores.Events.Add(data.Event{
-			EventSourceDeviceID: device.ID,
-			RequestDeviceID: "Unknown",
-			ResponseTimestamp: deviceState.Time,
-			EventTimestamp: eventTimestamp.Unix(),
-			FieldName: fieldName,
-			FieldValue: fmt.Sprint(dataMap["state"].(map[string]any)[fieldName]),
-		})
+	// Make request
+	for _, device := range devices {
+		deviceState, err := sensors.MakeYoLinkRequest[sensors.BUDP](yoLinkConnection, sensors.SimpleBDDP{Method: sensors.THSensorGetState, TargetDevice: &device.ID, Token: &device.Token})
+		if deviceState.Code != "000000" {
+			log.Default().Output(1, fmt.Sprintf("code was non-zero: %v for device %v (name: %v) at time %v", deviceState.Code, device.ID, device.Name, utils.Time()))
+		}
 		if err != nil {
-			return fmt.Errorf("error while adding event: %w", err)
+			return fmt.Errorf("error while quering device: %w", err)
+		}
+		// Process response
+		dataMap, err := utils.ToMap[any](deviceState.Data)
+		if err != nil {
+			return fmt.Errorf("error converting data %v: %w", deviceState.Data, err)
+		}
+		pairs := TraverseMap(dataMap, []KVPair{}, "")
+		fmt.Println(pairs)
+
+		// Ensure neccesary keys exist
+		var hasReportAt bool
+		for k := range dataMap {
+			if k == "reportAt" {
+				hasReportAt = true
+			}
+		}
+		if !hasReportAt {
+			logger := log.Default()
+			logger.Output(1, fmt.Sprintf("reportAt missing for sensor %v (name %v) at time %v", device.ID, device.Name, time.Now()))
+			continue
+		}
+
+		eventTimestamp, err := time.Parse(time.RFC3339Nano, dataMap["reportAt"].(string))
+		if err != nil {
+			return fmt.Errorf("error converting time %v to epoch seconds: %w", dataMap["reportAt"], err)
+		}
+		for _, pair := range pairs {
+			err := stores.Events.Add(data.Event{
+				EventSourceDeviceID: device.ID,
+				RequestDeviceID:     "1",
+				ResponseTimestamp:   deviceState.Time,
+				EventTimestamp:      eventTimestamp.Unix(),
+				FieldName:           pair.k,
+				FieldValue:          pair.v,
+			})
+			if err != nil {
+				return fmt.Errorf("error while adding event: %w", err)
+			}
 		}
 	}
-
 	return nil
+}
+
+type KVPair struct {
+	k string
+	v string
+}
+
+// Traverse a map m where all keys and nested keys are strings, and values are strings or maps of strings, adding all key value pairs to array a
+// keyPrefix will prefix all keys with the given string. External callers can provide ""
+func TraverseMap(m map[string]any, a []KVPair, keyPrefix string) []KVPair {
+	if keyPrefix != "" {
+		keyPrefix += "."
+	}
+	for k, v := range m {
+		switch v := v.(type) {
+		case map[string]any:
+			a = append(a, TraverseMap(v, []KVPair{}, keyPrefix+k)...)
+		default:
+			a = append(a, KVPair{k: keyPrefix + k, v: fmt.Sprint(v)})
+		}
+	}
+	return a
 }
